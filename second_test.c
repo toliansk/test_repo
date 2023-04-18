a660beb82719ab4b1a575f67eb502551dcc69bf0
toliansk <tolian.sk@gmail.com>
2023-04-18 17:19:26 +0300

    Squashind for Dolev

--- second_test.c
+++ second_test.c
@@ -1,6 +1,17 @@
 int main() {
 
 // Adding comment
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+   // printf() displays the string inside quotation
+
+   // printf() displays the string inside quotation
+
+
    printf("Hello, World!");
    return 0;
 }
